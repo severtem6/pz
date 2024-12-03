82b7871e0efb974ea1f626a8bb9a67886f34d001
severtem6 <vladislavsolopov@MacBook-Air.local>
2024-12-03 09:18:16 +0300

    add new site

--- js/script.js
+++ js/script.js
@@ -1,17 +1,16 @@
-document.querySelectorAll('.input-group input').forEach(input => {
-    input.addEventListener('focus', () => {
-        input.parentNode.classList.add('focused');
-    });
-    input.addEventListener('blur', () => {
-        input.parentNode.classList.remove('focused');
-    });
+document.querySelectorAll(".input-group input").forEach((input) => {
+  input.addEventListener("focus", () => {
+    input.parentNode.classList.add("focused");
+  });
+  input.addEventListener("blur", () => {
+    input.parentNode.classList.remove("focused");
+  });
 });
 
-
-const loginButton = document.querySelector('.login-button');
-loginButton.addEventListener('mouseover', () => {
-    loginButton.style.transform = 'scale(1.05)';
+const loginButton = document.querySelector(".login-button");
+loginButton.addEventListener("mouseover", () => {
+  loginButton.style.transform = "scale(1.05)";
 });
-loginButton.addEventListener('mouseout', () => {
-    loginButton.style.transform = 'scale(1)';
+loginButton.addEventListener("mouseout", () => {
+  loginButton.style.transform = "scale(1)";
 });
